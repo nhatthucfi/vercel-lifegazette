03424531df15eedd040513f484281def9c797ac1
nhatthucfi <52905500+nhatthucfi@users.noreply.github.com>
2023-12-28 20:14:05 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,7 +55,7 @@ const Home: NextPage = () => {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image src="https://lifegazette.info/wp-content/uploads/2023/09/cropped-lifegazette-logo-1.png" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
