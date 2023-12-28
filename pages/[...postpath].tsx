a7af9390d636bd1e954c901c747e9d499f4e53d0
nhatthucfi <52905500+nhatthucfi@users.noreply.github.com>
2023-12-28 22:57:13 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					endpoint.replace(/(\/graphql\/)/, '/') + encodeURI(path as string)
+					`https://lifegazette.info/` + encodeURI(path as string)
 				}`,
 			},
 		};
