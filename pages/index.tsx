358ef683c875b9a8a91fdaeabba5a5068bb042c0
nhatthucfi <52905500+nhatthucfi@users.noreply.github.com>
2023-12-28 13:51:08 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Vercel Trick By <a href="https://alcashzone.com">Alcash Zone</a>
+          Welcome to Vercel Trick By <a href="https://lifegazette.info">Alcash Zone</a>
         </h1>
 
         <p className={styles.description}>
@@ -23,18 +23,18 @@ const Home: NextPage = () => {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://alcashzone.com/category/adsterra-earning/" className={styles.card}>
+          <a href="https://lifegazette.info/category/adsterra-earning/" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Here you can find how to earn money from Adsterra .</p>
           </a>
 
-          <a href="https://alcashzone.com/category/adsense-approval/" className={styles.card}>
+          <a href="https://lifegazette.info/category/adsense-approval/" className={styles.card}>
             <h2>Master In AdSense Approval &rarr;</h2>
             <p>Here you can find the of how you become master in AdSense approval!</p>
           </a>
 
           <a
-            href="https://alcashzone.com/category/blogging/"
+            href="https://lifegazette.info/category/blogging/"
             className={styles.card}
           >
             <h2>Blogging &rarr;</h2>
