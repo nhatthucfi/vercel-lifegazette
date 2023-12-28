c7ac59e2ac02c13861ff94fd7a911337dfba71e4
nhatthucfi <52905500+nhatthucfi@users.noreply.github.com>
2023-12-28 20:28:37 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,7 +49,7 @@ const Home: NextPage = () => {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://lifegazette.info"
           target="_blank"
           rel="noopener noreferrer"
         >
