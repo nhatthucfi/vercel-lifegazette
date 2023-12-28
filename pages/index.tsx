c6f3d72563af5d2413d6684befcafb699265c0c6
nhatthucfi <52905500+nhatthucfi@users.noreply.github.com>
2023-12-28 18:56:28 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Vercel Trick By <a href="https://lifegazette.info">lifegazette</a>
+          Welcome to Vercel By <a href="https://lifegazette.info">lifegazette</a>
         </h1>
 
         <p className={styles.description}>
