d05b140a483b85c0ccba10745faeaf46a0accce6
nhatthucfi <52905500+nhatthucfi@users.noreply.github.com>
2024-07-02 16:30:56 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://lifegazette.info/graphql"
+	const endpoint = "https://lifenew24h.net/graphql/"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://lifegazette.info/` + encodeURI(path as string)
+					`https://lifenew24h.net/` + encodeURI(path as string)
 				}`,
 			},
 		};
