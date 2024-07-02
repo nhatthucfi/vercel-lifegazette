d2e52a441ea9e383f6bb502f44e4428e0776c16b
nhatthucfi <52905500+nhatthucfi@users.noreply.github.com>
2024-07-02 23:14:30 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,34 +14,40 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://lifegazette.info">lifegazette</a>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.tsx</code>
+        </p>
+
         <div className={styles.grid}>
-          <a href="https://lifegazette.info/topics/baby/" className={styles.card}>
-            <h2> Baby &rarr;</h2>
-            <p> Here you can find other domestic and international news about babies.</p>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h2>Documentation &rarr;</h2>
+            <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://lifegazette.info/topics/pet/" className={styles.card}>
-            <h2> Dog &rarr;</h2>
-            <p> Here you can find other national and international news about dogs </p>
-          </a>
-
-          <a
-            href="https://lifegazette.info/category/blogging/"
-            className={styles.card}
-          >
-            <h2>Blogging &rarr;</h2>
-            <p>Here you can find the source to become master in blogging</p>
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h2>Learn &rarr;</h2>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
-            href="https://lifegazette.info"
+            href="https://github.com/vercel/next.js/tree/canary/examples"
             className={styles.card}
           >
-            <h2>News &rarr;</h2>
+            <h2>Examples &rarr;</h2>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h2>Deploy &rarr;</h2>
             <p>
-              Here you can find showviz news and other national and internation news
+              Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
         </div>
@@ -49,7 +55,7 @@ const Home: NextPage = () => {
 
       <footer className={styles.footer}>
         <a
-          href="https://lifegazette.info"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
